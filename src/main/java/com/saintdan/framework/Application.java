4b4ae52f7a5728edeb5073f6654151245ff8193b
Liao Yifan <saintdan1011@gmail.com>
2016-01-29 13:56:47 +0800

    0.9.6.RELEASE

--- src/main/java/com/saintdan/framework/Application.java
+++ src/main/java/com/saintdan/framework/Application.java
@@ -1,7 +1,12 @@
 package com.saintdan.framework;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.PropertyPlaceholderAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
+import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.*;
+import org.springframework.context.annotation.Import;
 
 /**
  * Entrance of <p>spring-rest-oauth2</p>
@@ -14,6 +19,22 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @since JDK1.8
  */
 @SpringBootApplication
+// I don't use Jmx and web socket, so I comment them.
+@Import({
+        DispatcherServletAutoConfiguration.class,
+        EmbeddedServletContainerAutoConfiguration.class,
+        ErrorMvcAutoConfiguration.class,
+        HttpEncodingAutoConfiguration.class,
+        HttpMessageConvertersAutoConfiguration.class,
+        JacksonAutoConfiguration.class,
+//        JmxAutoConfiguration.class,
+        MultipartAutoConfiguration.class,
+        ServerPropertiesAutoConfiguration.class,
+        PropertyPlaceholderAutoConfiguration.class,
+        ThymeleafAutoConfiguration.class,
+        WebMvcAutoConfiguration.class,
+//        WebSocketAutoConfiguration.class,
+})
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
