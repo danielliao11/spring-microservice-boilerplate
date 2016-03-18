f895f6d75c3902bfc967897fc948c93a64dc4856
Liao Yifan <saintdan1011@gmail.com>
2016-03-18 15:44:02 +0800

    Delete update valid flag.

--- src/main/java/com/saintdan/framework/repo/RepositoryWithoutDelete.java
+++ src/main/java/com/saintdan/framework/repo/RepositoryWithoutDelete.java
@@ -1,14 +1,10 @@
 package com.saintdan.framework.repo;
 
 
-import com.saintdan.framework.enums.ValidFlag;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.NoRepositoryBean;
 import org.springframework.data.repository.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.io.Serializable;
 
