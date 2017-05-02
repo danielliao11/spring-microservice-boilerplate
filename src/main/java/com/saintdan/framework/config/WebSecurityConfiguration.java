53522c31b8f0518749644889a9edc5283b8f6a2f
Liao Yifan <saintdan1011@gmail.com>
2017-05-02 15:16:50 +0800

    [MOD] Modify injection.

--- src/main/java/com/saintdan/framework/config/WebSecurityConfiguration.java
+++ src/main/java/com/saintdan/framework/config/WebSecurityConfiguration.java
@@ -1,14 +1,7 @@
 package com.saintdan.framework.config;
 
-import com.saintdan.framework.component.CustomPasswordEncoder;
-import com.saintdan.framework.component.LogHelper;
 import com.saintdan.framework.config.custom.CustomAuthenticationProvider;
-import com.saintdan.framework.domain.UserDomain;
-import com.saintdan.framework.repo.OauthAccessTokenRepository;
-import com.saintdan.framework.repo.OauthRefreshTokenRepository;
-import com.saintdan.framework.repo.UserRepository;
 import com.saintdan.framework.tools.Assert;
-import javax.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
@@ -29,35 +22,11 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration @EnableWebSecurity @EnableSpringDataWebSupport
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-  private final UserRepository userRepository;
+  private final CustomAuthenticationProvider customAuthenticationProvider;
 
-  private final OauthAccessTokenRepository accessTokenRepository;
-
-  private final OauthRefreshTokenRepository refreshTokenRepository;
-
-  private final HttpServletRequest request;
-
-  private final UserDomain userDomain;
-
-  private final LogHelper logHelper;
-
-  private final CustomPasswordEncoder customPasswordEncoder;
-
-  public WebSecurityConfiguration(UserRepository userRepository, OauthAccessTokenRepository accessTokenRepository, OauthRefreshTokenRepository refreshTokenRepository, HttpServletRequest request, UserDomain userDomain, LogHelper logHelper, CustomPasswordEncoder customPasswordEncoder) {
-    Assert.defaultNotNull(userRepository);
-    Assert.defaultNotNull(accessTokenRepository);
-    Assert.defaultNotNull(refreshTokenRepository);
-    Assert.defaultNotNull(request);
-    Assert.defaultNotNull(userDomain);
-    Assert.defaultNotNull(logHelper);
-    Assert.defaultNotNull(customPasswordEncoder);
-    this.userRepository = userRepository;
-    this.accessTokenRepository = accessTokenRepository;
-    this.refreshTokenRepository = refreshTokenRepository;
-    this.request = request;
-    this.userDomain = userDomain;
-    this.logHelper = logHelper;
-    this.customPasswordEncoder = customPasswordEncoder;
+  public WebSecurityConfiguration(CustomAuthenticationProvider customAuthenticationProvider) {
+    Assert.defaultNotNull(customAuthenticationProvider);
+    this.customAuthenticationProvider = customAuthenticationProvider;
   }
 
   @Override protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -69,7 +38,7 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
   }
 
   @Bean public AuthenticationProvider authenticationProvider() {
-    return new CustomAuthenticationProvider(userRepository, accessTokenRepository, refreshTokenRepository, request, userDomain, logHelper, customPasswordEncoder);
+    return customAuthenticationProvider;
   }
 
 }
