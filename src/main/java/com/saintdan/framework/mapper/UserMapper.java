40b3dd8a97bb375dfd0fac326ab2411997807f0f
saintdan <saintdan1011@gmail.com>
2019-02-20 23:43:14 +0800

    - FIX: Fix mapper bug.

--- src/main/java/com/saintdan/framework/mapper/UserMapper.java
+++ src/main/java/com/saintdan/framework/mapper/UserMapper.java
@@ -3,7 +3,6 @@ package com.saintdan.framework.mapper;
 import com.saintdan.framework.po.User;
 import com.saintdan.framework.tools.CommonMapper;
 import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
 
 /**
  * @author <a href="http://github.com/saintdan">Liao Yifan</a>
@@ -12,10 +11,5 @@ import org.apache.ibatis.annotations.Select;
  */
 public interface UserMapper extends CommonMapper<User> {
 
-  @Select("select u.id, u.name, u.usr, u.pwd, u.is_account_non_expired_alias, u.is_account_non_locked_alias, u.is_credentials_non_expired_alias, u.is_enabled_alias, u.status, u.description, u.last_login_at, u.ip, u.created_at, u.created_by, u.last_modified_at, u.last_modified_by, u.version, u.authority_str " +
-      "from users u " +
-      "left join accounts a " +
-      "on u.id = a.user_id " +
-      "where a.account = #{usr} and u.status = #{status}")
   User findByUsr(@Param("usr") String usr, @Param("status") Integer status);
 }
