0e8a4270b328180db13f74174b90f88dd1533826
saintdan <saintdan1011@gmail.com>
2019-04-08 23:31:57 +0800

    ADD: Add sign in

---
+++ web/src/styles/theme.js
@@ -0,0 +1,42 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme(
+  {
+    palette: {
+      common: {
+        black: '#000',
+        white: '#fff',
+      },
+      background: {
+        paper: '#fff',
+        default: '#fafafa',
+      },
+      primary: {
+        light: '#7986cb',
+        main: '#3f51b5',
+        dark: '#303f9f',
+        contrastText: '#fff',
+      },
+      secondary: {
+        light: '#ff4081',
+        main: '#f50057',
+        dark: '#c51162',
+        contrastText: '#fff',
+      },
+      error: {
+        light: '#e57373',
+        main: '#f44336',
+        dark: '#d32f2f',
+        contrastText: '#fff',
+      },
+      text: {
+        primary: 'rgba(0, 0, 0, 0.87)',
+        secondary: 'rgba(0, 0, 0, 0.54)',
+        disabled: 'rgba(0, 0, 0, 0.38)',
+        hint: 'rgba(0, 0, 0, 0.38)',
+      },
+    },
+  },
+);
+
+export default theme;
